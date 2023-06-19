e6be6e67bf2611e55ba77af0a559b406a205eb48
abdo harp <abdoharp234@gmail.com>
2023-06-19 14:54:17 +0300

    update code in admin middelewere

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class Admin
@@ -16,7 +17,14 @@ class Admin
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (!auth()->check() || auth()->user()->is_Admin) {
+        // if (!auth()->check() || auth()->user()->is_admin) {
+        //     abort(403);
+        // }
+        if (Auth::user()->is_admin == '1') {
+            return $next($request);
+        } else {
+            // session()->flush();
+            // return redirect()->route('login');
             abort(403);
         }
         return $next($request);
