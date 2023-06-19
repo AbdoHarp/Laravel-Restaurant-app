7dc7da0eaeae4dbf783305baebe0a12e3da9aea4
abdo harp <abdoharp234@gmail.com>
2023-06-19 15:08:32 +0300

    add route for the page in dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\foldersfordash\CategoryController;
+use App\Http\Controllers\Admin\foldersfordash\MenuController;
+use App\Http\Controllers\Admin\foldersfordash\ReservationController;
+use App\Http\Controllers\Admin\foldersfordash\TableController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -31,10 +35,10 @@ Route::middleware('auth')->group(function () {
 
 Route::middleware(['auth', 'admin'])->name('admin.')->prefix('admin')->group(function () {
     Route::get('/', [AdminController::class, 'index'])->name('index');
-    // Route::resource('/categories', CategoryController::class);
-    // Route::resource('/menus', MenuController::class);
-    // Route::resource('/tables', TableController::class);
-    // Route::resource('/reservations', ReservationController::class);
+    Route::resource('/categories', CategoryController::class);
+    Route::resource('/menus', MenuController::class);
+    Route::resource('/tables', TableController::class);
+    Route::resource('/reservations', ReservationController::class);
 });
 
 require __DIR__ . '/auth.php';
