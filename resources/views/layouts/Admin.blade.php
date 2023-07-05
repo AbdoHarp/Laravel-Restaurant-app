d53a6036ce9753698178a5a29f1d53220255684f
abdo harp <abdoharp234@gmail.com>
2023-07-05 14:11:53 +0300

    make route for file

--- resources/views/layouts/Admin.blade.php
+++ resources/views/layouts/Admin.blade.php
@@ -19,7 +19,7 @@
 <body class="font-sans antialiased">
     <div class="flex-col w-full md:flex md:flex-row md:min-h-screen">
         <div @click.away="open = false"
-            class="flex flex-col flex-shrink-0 w-full text-gray-700 bg-white md:w-64 dark-mode:text-gray-200 dark-mode:bg-gray-800"
+            class="flex flex-col flex-shrink-0 w-full text-gray-700 bg-slate-100 md:w-64 dark-mode:text-gray-200 dark-mode:bg-gray-800"
             x-data="{ open: false }">
             <div class="flex flex-row items-center justify-between flex-shrink-0 px-8 py-4">
                 <a href={{ url('/admin') }}
